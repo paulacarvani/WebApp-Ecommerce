b35994b795e1380d05e780e1b2f1ce69bf886d0c
paulacarvani <3312@holbertonschool.com>
2022-03-03 01:04:25 -0500

    Add a Slider in the landing page

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/ncPCYB0/Untitled-design.png",
+        title: "NEW COLLECTION",
+        desc: "Handmade Decoration",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/dftsHgV/Untitled-design-1.png",
+        title: "SUPERHEROES COLLECTION",
+        desc: "What's your favorite?",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/mSN0LGx/Untitled-design-2.png",
+        title: "VINTAGE COLLECTION",
+        desc: "You think it, we do it",
+    },
+]
